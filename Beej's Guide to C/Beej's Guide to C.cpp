2de6ba6d29515f48a196d7d90bf536e060dd8433
Jad Driggers <jaddriggers@gmail.com>
2020-09-15 13:44:13 -0400

    Add project files.

---
+++ Beej's Guide to C/Beej's Guide to C.cpp
@@ -0,0 +1,17 @@
+// Beej's Guide to C.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+
+	i = 2;
+
+	printf(" %d\n", i);
+
+	return 0;
+}
